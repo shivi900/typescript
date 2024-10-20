2aa81f4e8628d617107c333a0b2eb514162c8626
shivam.lathor43@gmail.com <shivam.lathor4>
2024-10-20 23:44:04 +0530

    minor changes in UI

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+import React,{useEffect} from 'react';
+import { ApolloClient, InMemoryCache, ApolloProvider,gql } from '@apollo/client';
 import awsconfig from '../typescript/src/aws-exports';
 import UserScreen from './src/UserScreen';
 
@@ -12,6 +12,48 @@ const client = new ApolloClient({
 });
 
 const App = () => {
+  useEffect(() => {
+    const testGraphQLEndpoint = async () => {
+      const endpoint = awsconfig.aws_appsync_graphqlEndpoint; 
+      const LIST_USERS_QUERY = gql`
+  query ListZellerCustomers {
+    listZellerCustomers(filter: { role: { eq: "Manager" } }) {
+      items {
+        id
+        name
+        role
+      }
+    }
+  }
+`;
+    
+      try {
+        const response = await fetch(endpoint, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+            'x-api-key': awsconfig.aws_appsync_apiKey, // If you're using an API key
+          },
+          body: JSON.stringify({
+            query: LIST_USERS_QUERY
+          }),
+        });
+
+        const data = await response.json();
+        console.log('Response from GraphQL endpoint:', data);
+
+        if (response.ok) {
+          console.log('Endpoint is working correctly.');
+        } else {
+          console.log('Endpoint returned an error:', data);
+        }
+      } catch (error) {
+        console.error('Error reaching GraphQL endpoint:', error);
+      }
+    };
+
+    testGraphQLEndpoint();
+  }, []);
   return (
     <ApolloProvider client={client}>
       <UserScreen />
